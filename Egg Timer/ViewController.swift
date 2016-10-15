1ffc96c918b115ccd4bf733809574f84d6b91490
Tad <terry.donaghe@gmail.com>
2016-10-15 11:37:40 -0700

    First commit

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,7 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timeLeft = 210
+    var timer = Timer()
+    
+    @IBOutlet weak var lblTimeLeft: UILabel!
 
+    @IBAction func btnGo(_ sender: AnyObject) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func btnPause(_ sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func btnReset(_ sender: AnyObject) {
+        timeLeft = 210
+        lblTimeLeft.text = "210"
+    }
+    
+    @IBAction func btnAddTen(_ sender: AnyObject) {
+        timeLeft += 10
+        lblTimeLeft.text = String(timeLeft)
+    }
+    
+    @IBAction func btnSubtractTen(_ sender: AnyObject) {
+        if timeLeft - 10 > 0 {
+            timeLeft -= 10
+            lblTimeLeft.text = String(timeLeft)
+        } else {
+            timeLeft = 0
+            lblTimeLeft.text = "0"
+        }
+    }
+    
+    func processTimer() {
+        if timeLeft > 1 {
+            timeLeft -= 1
+            lblTimeLeft.text = String(timeLeft)
+        } else {
+            lblTimeLeft.text = "Done!!"
+            timer.invalidate()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
